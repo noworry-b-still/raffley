edaf51cd2116510b17c9e0d02077123b59de06f6
noworry-b-still <teslaatozinspires7@gmail.com>
2024-11-03 15:52:45 -0500

    add rules - use heex, tailwind

---
+++ lib/raffley/rules.ex
@@ -0,0 +1,18 @@
+defmodule Raffley.Rules do
+  def list_rules() do
+    [
+      %{
+        id: 1,
+        text: "Participants should have high tolerance for puns and dank jokes"
+      },
+      %{
+        id: 2,
+        text: "Winner must do a victory dance when claiming their prize"
+      },
+      %{
+        id: 3,
+        text: "have fun"
+      }
+    ]
+  end
+end
---
+++ lib/raffley_web/controllers/rule_controller.ex
@@ -0,0 +1,10 @@
+defmodule RaffleyWeb.RuleController do
+  use RaffleyWeb, :controller
+  alias Raffley.Rules
+
+  def index(conn, _params) do
+    emojis = ~w(✌🏽 ✅ 👨🏻‍💻) |> Enum.random() |> String.duplicate(5)
+    rules = Rules.list_rules()
+    render(conn, :index, emojis: emojis, rules: rules)
+  end
+end
