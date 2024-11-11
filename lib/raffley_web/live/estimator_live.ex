dee5c5fe97e42f9be4697fff773c5959a66bc961
noworry-b-still <teslaatozinspires7@gmail.com>
2024-11-10 20:50:22 -0500

    add estimator live page

---
+++ lib/raffley_web/live/estimator_live.ex
@@ -0,0 +1,32 @@
+defmodule RaffleyWeb.EstimatorLive do
+  use RaffleyWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    socket = assign(socket, tickets: 0, price: 3)
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div class="estimator">
+      <h1>Raffle Estimator</h1>
+      <section>
+        <div>
+          <%= @tickets %>
+        </div>
+        @
+        <div>
+          <%= @price %>
+        </div>
+        =
+        <div>
+          <%= @tickets * @price %>
+        </div>
+      </section>
+    </div>
+    """
+  end
+
+  # def handle_event(event, unsigned_params, socket) do
+  # end
+end
